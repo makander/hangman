0e1665ec2a17f0599f040f75b75ad1b89c0996a6
Nils Makander <makander@gmail.com>
2018-11-05 01:23:14 +0100

    add restart button and reset function

--- js/script.js
+++ js/script.js
@@ -13,20 +13,119 @@ var startTime; // Mäter tiden
 var numberOfGuesses = 0;
 var successfulGuesses = [];
 var restartButton = document.querySelector('#restartBtn')
+var gameBoard = document.querySelector('#gameBoard')
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
 // Initiering av globala variabler samt koppling av funktioner till knapparna.
 
 function init() {
-    startGameBtn.addEventListener('click', createBoxes);
+    
+    startGameBtn.addEventListener('click', function() {
+        createBoxes();
+        gameBoardVisible();
+        startGameBtnToggle();
+    });
     restartBtn.style.visibility = 'hidden';
-    restartBtn.addEventListener('click',) { wordGenerator,createBoxes };
+    
 };
+
+function gameBoardVisible () {
+    gameBoard.style.visibility = 'visible';
+}
+
+//function restartBtn
+
+
+function startGameBtnToggle () {
+        if ( startGameBtn.style.visibility == 'hidden' ) {
+            startGameBtn.style.visibility = 'visible'; 
+        } else  {
+            startGameBtn.style.visibility == 'hidden';
+        }  
+}
+
+function restartBtnToggle () {
+    if ( restartButton.style.visibility == 'hidden' ) {
+        restartButton.style.visibility = 'visible'; 
+    } else  {
+        restartButton.style.visibility = 'hidden';
+    }  
+}
+
 
 window.onload = init; 
 
+/* function startGame () {
+    return gameBoardVisible();
+    return createBoxes();
+}
+ */
+
+function resetImageandGuess () {
+    numberOfGuesses = 0;
+    hangmanImg.src=`images/h${numberOfGuesses}.png`;
+}
+
+
+restartBtn.addEventListener('click', function() {
+    removeLetterBoxes(); 
+    enableButton();
+    removeMsg();
+    resetImageandGuess();
+    startGameBtnToggle();
+    restartBtnToggle();
+});
+
+
+
 function wordGenerator () {
     return wordList[Math.floor(Math.random()*wordList.length)];
+}
+
+/* function userMessage () {
+    var msgBox = document.createElement('h1');
+    var msgBoxContent = document.createTextNode('You +')
+    msgBox.appendChild(msgBoxContent)
+    msgElem.appendChild(msgBox);
+    restartBtn.style.visibility = 'visible';
+}
+
+*/
+
+function removeLetterBoxes() {
+    while (letterBoxes.firstChild) {
+        letterBoxes.removeChild(letterBoxes.firstChild);
+    }
+}
+
+function removeMsg () {
+        msgElem.removeChild(msgElem.firstChild);
+    };
+
+function looseMsg () {
+    var msgBox = document.createElement('h1');
+    var msgBoxContent = document.createTextNode('YOU LOOSE!')
+    msgBox.appendChild(msgBoxContent)
+    msgElem.appendChild(msgBox);
+}
+
+function winMsg () {
+    var msgBox = document.createElement('h1');
+    var msgBoxContent = document.createTextNode('YOU WIN!')
+    msgBox.appendChild(msgBoxContent)
+    msgElem.appendChild(msgBox);
+}
+
+function enableButton () {
+letterButtons.forEach(button => {
+    button.disabled = false;
+});
+}
+
+function disableButton () {
+    letterButtons.forEach(button => {
+        button.disabled = true;
+    });
 }
 
 // Add list letterBoxes and create box ids 
@@ -39,8 +138,9 @@ for (let index = 0; index < splitRandomWord.length; index++) {
   letterBox.setAttribute('id', + [index]);
   li.appendChild(letterBox);
   letterBoxes.appendChild(li);
-  startGameBtn.style.visibility = 'hidden';
+  //startGameBtn.style.visibility = 'hidden';
   }};
+
 
 letterButtons.forEach(function(button) {
 	button.addEventListener('click',function(e){
@@ -54,10 +154,11 @@ letterButtons.forEach(function(button) {
                 successfulGuesses.push(buttonVal)
                 if (successfulGuesses.length === splitRandomWord.length) {
                     var msgBox = document.createElement('h1');
-                    var msgBoxContent = document.createTextNode('YOU WIN!')
+                    var msgBoxContent = document.createTextNode('YOU WIN!');
                     msgBox.appendChild(msgBoxContent)
                     msgElem.appendChild(msgBox);
-                    restartBtn.style.visibility = 'visible';
+                    restartBtnToggle();
+                    disableButton();
                 }
                 wrongGuess = false; 
             }  
@@ -66,12 +167,13 @@ letterButtons.forEach(function(button) {
                     numberOfGuesses++;
                     hangmanImg.src=`images/h${numberOfGuesses}.png`;
                     wrongGuess = false;
-                    if (numberOfGuesses > 6) {
+                    if (numberOfGuesses > 5) {
                         var msgBox = document.createElement('h1');
                         var msgBoxContent = document.createTextNode('YOU LOOSE!')
                         msgBox.appendChild(msgBoxContent)
                         msgElem.appendChild(msgBox);
-                        restartBtn.style.visibility = 'visible';
+                        restartBtnToggle();
+                        disableButton();
                         }
         }
         button.disabled = true;
@@ -81,4 +183,4 @@ letterButtons.forEach(function(button) {
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 // Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
-// Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
+// Funktionen ropas vid vinst eller förlust, gör olika saker beroende av de // 
